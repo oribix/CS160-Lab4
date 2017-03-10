077be2a2aced6fba07fb0f68f9aea3659c7cbe24
Marco Morelos <mmore019@well.cs.ucr.edu>
2017-03-09 21:56:03 -0800

    modified:   .gitignore
    added .swp
    
    new file:   1.c
    new file:   makefile
    new file:   matmul.c

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "omp.h"
+
+int main(){
+  int ID = 0;
+  printf("hello(%d)", ID);
+  printf("world(%d)\n", ID);
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+all:
+	gcc 1.c -o question1.out
+
+clean:
+	rm -f *.out
