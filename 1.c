93ea5f8cd841190330b8925cf2fc207bc1727405
Marco Morelos <mmore019@well.cs.ucr.edu>
2017-03-10 01:08:54 -0800

    modified:   1.c
    now has sequential pi program
    
    modified:   makefile
    
    removed "run"

--- 1.c
+++ 1.c
@@ -1,13 +1,27 @@
 #include "stdlib.h"
 #include "stdio.h"
-#include "unistd.h"
 #include "omp.h"
 
+static long numSteps = 100000;
+double step;
+
 int main(){
-  #pragma omp parallel num_threads(4)
-  {
-    int ID = omp_get_thread_num();
-    printf("hello(%d) ", ID);
-    printf("world(%d)\n", ID);
+  int i;
+  double x, pi, sum = 0.0;
+
+  step = 1.0/(double)numSteps;
+
+  double start = omp_get_wtime();
+
+  for(i = 0; i < numSteps ; i++){
+    x = (i + 0.5) * step;
+    sum += 4.0/(1.0 + x * x);
   }
+  pi = step * sum;
+
+  double end = omp_get_wtime();
+
+  printf("time elapsed: %f seconds\n", end-start);
+
+  printf("pi = %1.16f\n", pi);
 }
--- makefile
+++ makefile
@@ -1,8 +1,5 @@
 all:
 	gcc -fopenmp 1.c -o question1.out
 
-run: all
-	./question1.out
-
 clean:
 	rm -f *.out
