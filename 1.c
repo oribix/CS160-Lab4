fa1ef0a47c9d0870b3cd1ea554c1decf4e68e6dc
Marco Morelos <mmore019@well.cs.ucr.edu>
2017-03-10 01:54:52 -0800

    modified:   1.c
    parallelized

--- 1.c
+++ 1.c
@@ -7,16 +7,29 @@ double step;
 
 int main(){
   int i;
-  double x, pi, sum = 0.0;
+  double pi, sum = 0.0;
+  double partialsum[numSteps];
 
   step = 1.0/(double)numSteps;
 
+  omp_set_num_threads(4);
   double start = omp_get_wtime();
 
-  for(i = 0; i < numSteps ; i++){
-    x = (i + 0.5) * step;
-    sum += 4.0/(1.0 + x * x);
+  #pragma omp parallel
+  {
+    int numThreads = omp_get_num_threads();
+    int id = omp_get_thread_num();
+    int i;
+    double x;
+    for(i = id; i < numSteps ; i += numThreads){
+      x = (i + 0.5) * step;
+      partialsum[i] = 4.0/(1.0 + x * x);
+    }
   }
+
+  for(i = 0; i < numSteps; i++)
+    sum += partialsum[i];
+
   pi = step * sum;
 
   double end = omp_get_wtime();
